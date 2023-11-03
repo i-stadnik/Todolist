e2c480c8dae28342b86e7c2146145b363f642ae2
Igor Stadnik <i.stadnik.dev@gmail.com>
2023-11-03 12:54:14 +0200

    added Tailwind CSS and Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
 function App() {
-  return (
-    <main className="container mx-auto px-4 pt-12">
-      <div>
-        <h1>dfd</h1>
-      </div>
-    </main>
-  )
+	return (
+		<main className='container mx-auto px-4 pt-12'>
+			<div>
+				<h1>Todolist React JS</h1>
+				<div className='gap-8 columns-2 pt-10'>
+					<div className='a'>
+						<p>
+							Quisquam per sollicitudin viverra scelerisque pretium tellus
+							vivamus veniam suspendisse. Pellentesque, pulvinar rem? Arcu, illo
+							quibusdam! Odit cillum! Rem distinctio torquent ad accusamus
+							corporis netus.
+						</p>
+
+						<p>
+							Ullam! Sit ornare fermentum viverra aspernatur conubia voluptas
+							penatibus cursus, lorem eleifend ad imperdiet leo morbi temporibus
+							duis cupiditate eu! Excepteur occaecati nobis natoque. Sociosqu.
+						</p>
+					</div>
+					<div className='a'>
+						<p>
+							Esse diamlorem accusamus gravida ex? Posuere nam non porro
+							incididunt praesent optio tristique molestiae? Nostrud dictumst,
+							condimentum explicabo tempore class, quibusdam justo, magnis quod!
+							Dui.
+						</p>
+					</div>
+					<div className='a'>
+						<p>
+							Rem est quos laudantium! Itaque rem ipsam porta! Tempor malesuada
+							harum lobortis, perferendis officia mus, dolorum, sapien aliqua,
+							facere praesent? Praesentium, cum dictum dictum voluptas.
+						</p>
+					</div>
+				</div>
+			</div>
+		</main>
+	)
 }
 
 export default App
